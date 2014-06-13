9482e873aa7904bddab6c00eaec82e9ef2565bec
BeachPanda <david_cerdeira11@hotmail.com>
2014-06-13 21:35:34 +0100

    Create Micro_Other.c

---
+++ Micro_Other.c
@@ -0,0 +1,42 @@
+#include <at89c51ic2.h>
+
+int twiData;
+bit b_TWI_busy = 0;
+
+unsigned char vec[] = {12,0xcf,0x54,0x46,0x87,0x26,0x24,0x4f,0x04, 0x07};
+
+void main()
+{
+   	IEN1 |= 0x02; //enable interrupt
+	SSADR = 0x55 << 1; //slave adress
+	SSCON |= 0x44;	//activate TWI & AA
+	EA = 1;	    //enable all
+
+	while(1);
+}
+
+void twi_slave_receiver() interrupt 8
+{
+	if(SSCS == 0x60){	 //Own SLA+W has been received; ACK has been returned
+	   					 //Data byte will be received and ACK will be returned
+	   SSCON |= 0x04;	 //set AA
+	   P1 = vec[0];
+	}
+	else if(SSCS == 0x80){	//Previously addressed with own SLA+W; data has been received; ACK has been returned
+	   SSCON |= 0x04;		//Data byte will be received and ACK will be returned
+		P1 = vec[1];
+	}
+	else if(SSCS == 0x88){	//Previously addressed with own SLA+W; data has been received; NOT ACK has been returned
+	   twiData = SSDAT;		//Switched to the not addressed slave mode; own SLA will be recognised; GCA will be recognised if GC=logic 
+	   P1 = twiData;
+	   SSCON |= 0x04;
+	   b_TWI_busy = 0;		//Switched to the not addressed slave mode; own SLA will be recognised; GCA will be recognised if GC=logic 1
+		P1 = vec[2];
+	}
+	else if(SSCS == 0xA0){	//A STOP condition or repeated START condition has been received while still addressed as slave
+	   SSCON |= 0x04;
+	    b_TWI_busy = 0;
+		P1 = vec[3];
+	}
+	SSCON &= ~0x08;                  /* clear flag */
+}
